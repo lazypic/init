d55a4303d6e91eef16de21950fad79735d203f80
kimhanwoong <khw7096@gmail.com>
2015-10-26 22:26:52 +0900

    add cmd g with searchword

--- init.bash
+++ init.bash
@@ -24,30 +24,34 @@ go() {
 
 #dependency command for other OS.
 if [[ `uname` == 'Linux' ]] || [[ `uname` == 'MINGW'* ]]; then
-	alias browser="firefox"
+	browser="firefox"
 elif [[ `uname` == 'Darwin' ]]; then
-	alias browser="open"
+	browser="open"
 fi
 
-#connect url
+#google search
+g() {
+	$browser "http://www.google.com/search?q=$1";
+}
+
+#other connect url
 alias ff="find . -name $1"
-alias g="browser http://www.google.com"
-alias flameman="browser http://docs.autodesk.com/flamepremium2015"
-alias reddit="browser http://www.reddit.com/r/golang"
-alias wacom="browser http://www.wacom.com/en-us/support/intuos-pro-support-detail"
-alias cloudb="browser http://cloud.blender.org"
-alias vimeo="browser https://vimeo.com/channels/lazypic"
-alias youtube="browser http://www.youtube.com"
-alias soundcloud="browser https://soundcloud.com/groups/lazypic"
-alias gmail="browser http://www.gmail.com"
-alias imail="browser https://www.icloud.com"
-alias github="browser http://www.github.com"
-alias trending="browser http://www.github.com/trending"
-alias bitbucket="browser http://www.bitbucket.org"
-alias gitbook="browser http://www.gitbook.com"
-alias wiki="browser http://sites.google.com/site/lazypicweb"
-alias homepage="browser http://lazyd.org"
-alias googledrive="browser https://drive.google.com/drive/my-drive"
-alias godoc="browser https://golang.org/pkg/"
-alias baidu="browser https://www.baidu.com"
+alias flameman="$browser http://docs.autodesk.com/flamepremium2015"
+alias reddit="$browser http://www.reddit.com/r/golang"
+alias wacom="$browser http://www.wacom.com/en-us/support/intuos-pro-support-detail"
+alias cloudb="$browser http://cloud.blender.org"
+alias vimeo="$browser https://vimeo.com/channels/lazypic"
+alias youtube="$browser http://www.youtube.com"
+alias soundcloud="$browser https://soundcloud.com/groups/lazypic"
+alias gmail="$browser http://www.gmail.com"
+alias imail="$browser https://www.icloud.com"
+alias github="$browser http://www.github.com"
+alias trending="$browser http://www.github.com/trending"
+alias bitbucket="$browser http://www.bitbucket.org"
+alias gitbook="$browser http://www.gitbook.com"
+alias wiki="$browser http://sites.google.com/site/lazypicweb"
+alias homepage="$browser http://lazyd.org"
+alias googledrive="$browser https://drive.google.com/drive/my-drive"
+alias godoc="$browser https://golang.org/pkg/"
+alias baidu="$browser https://www.baidu.com"
 
