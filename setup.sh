8d5c2788b9f79965ef3407bb7ae8c38400b10c83
hanwoong <khw7096@gmail.com>
2016-01-30 00:16:35 +0900

    add mingw64

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 
 if [[ `uname` == 'Linux' ]]; then
 	echo "source ~/init/init.bash" >> ~/.bashrc
+elif [[ `uname` == 'MINGW'* ]]; then
+	echo "source ~/init/init.bash" >> ~/.bashrc
 elif [[ `uname` == "Darwin" ]]; then
 	echo "source ~/init/init.bash" >> ~/.profile
 fi
