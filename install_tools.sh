517581cd5358da958a486eab00ba85d30aa1a517
kim hanwoong <khw7096@gmail.com>
2018-12-11 23:06:11 +0900

    ffmpeg 수정함.

--- install_tools.sh
+++ install_tools.sh
@@ -5,7 +5,6 @@ brew install guetzli # image optimization
 brew install pandoc
 brew install mplayer
 brew install mpv
-brew install ffmpeg
 brew install imagemagick
 brew install potrace # bitmap to vector
 brew install exiftool # edit image metadata
@@ -22,3 +21,37 @@ pip install pyopengl
 pip install pyside2
 pip install numpy
 brew install tesseract # get string from image
+
+# ffmpeg
+brew install ffmpeg --with-aom \
+--with-chromaprint \
+--with-fdk-aac \
+--with-fontconfig \
+--with-freetype \
+--with-frei0r \
+--with-game-music-emu \
+--with-libass \
+--with-libbluray \
+--with-libbs2b \
+--with-libcaca \
+--with-libgsm \
+--with-libmodplug \
+--with-librsvg \
+--with-libsoxr \
+--with-libssh \
+--with-libvidstab \
+--with-libvmaf \
+--with-opencore-amr \
+--with-openh264 \
+--with-openjpeg \
+--with-openssl \
+--with-rtmpdump \
+--with-rubberband \
+--with-speex \
+--with-srt \
+--with-tesseract \
+--with-two-lame \
+--with-wavpack \
+--with-webp \
+--with-zeromq \
+--with-zimg
