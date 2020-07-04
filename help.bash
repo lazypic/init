2253a02255dd536ace89fad510cfe9643c1928fa
woong <woong@lazypic.org>
2020-07-04 14:56:42 +0900

    환경셋팅

--- help.bash
+++ help.bash
@@ -1,11 +1,9 @@
 echo "=========== Lazypic Alias ==========="
-echo "    blender282 : run blender"
-echo "        natron : run natron"
+echo "    blender283 : run blender"
 echo "    renderfarm : sheep it renderfarm"
 echo "      homepage : open homepage"
 echo "        github : open github"
 echo "   googledrive : open googledrive"
-echo "       guetzli : image opimization"
 echo "        update : update setting"
 echo "====================================="
 
--- init.bash
+++ init.bash
@@ -46,44 +46,27 @@ if [ -d $HOME/Library/Python/3.7/bin ]; then
 	export PATH=$PATH:$HOME/Library/Python/3.7/bin
 fi
 
-# Mailgun
+# uri
 alias mailgun="$browser http://mailgun.com"
-
-
-#other connect url
 alias wacom="$browser http://www.wacom.com/en-us/support/intuos-pro-support-detail"
 alias cloudb="$browser http://cloud.blender.org"
 alias vimeo="$browser https://vimeo.com/channels/lazypic"
 alias youtube="$browser https://www.youtube.com/my_videos?ar=1&o=U"
-alias soundcloud="$browser https://soundcloud.com/groups/lazypic"
 alias gmail="$browser http://www.gmail.com"
 alias github="$browser http://www.github.com/lazypic"
-alias bugs="$browser https://music.bugs.co.kr"
-alias bitbucket="$browser http://www.bitbucket.org"
-alias gitbook="$browser http://www.gitbook.com"
 alias awsconsole="$browser https://console.aws.amazon.com"
-alias wasabi="$browser https://console.wasabisys.com"
-alias wasabisub="$browser https://console.wasabisys.com/#/subuserLogin"
 alias homepage="$browser http://lazypic.org"
 alias googledrive="$browser https://drive.google.com/drive/my-drive"
 alias conference_room="$browser http://www.goyang.go.kr/kr/facilities/main.do"
 alias optimizilla="$browser http://optimizilla.com/ko/"
 alias sheepit="java -jar ~/init/sheepit/sheepit-client-3.227.2399.jar"
 alias renderfarm="$browser https://www.sheepit-renderfarm.com/jobs.php?mode=add"
-alias weather="curl wttr.in/ilsan"
-alias jukedeck="$browser https://www.jukedeck.com"
-alias flickr="$browser https://www.flickr.com/photos/161844533@N02/"
-alias financialstatements="$browser https://docs.google.com/spreadsheets/d/1iwuiImg9T-V_05utVck3nrowu2Q0Nlrn3XiWz6Mgf84/edit#gid=0"
-alias editblog="$browser https://www.blogger.com/blogger.g?rinli=1&pli=1&blogID=8229706624590863176#allposts"
-alias brunch="$browser https://brunch.co.kr/write"
-alias slideshare="$browser https://www.slideshare.net"
 alias wiki="$browser https://github.com/lazypic/blender/wiki"
 alias clib="$browser https://www.youtube.com/channel/UC0L_YtB4PWSkOwp2m9587MA/playlists?view_as=subscriber"
-alias centos="ssh woong@192.168.219.106"
 alias rmgitbranch="git branch | grep -v "master" | xargs git branch -D"
 
 
-# Software & app
+# krita 경로설정
 krita="/Applications/krita.app/Contents/MacOS/krita"
 if [ -f $krita ]; then
 	alias krita=$krita
@@ -100,14 +83,6 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
 	alias blender282="$HOME/app/blender2.82/Blender --python $HOME/blender/init.py"
 	alias blender283="$HOME/app/blender2.83/Blender --python $HOME/blender/init.py"
 	alias blender290a="$HOME/app/blender2.90a/Blender --python $HOME/blender/init.py"
-fi
-
-# Natron
-if [ -d "$HOME/natron" ]; then
-	export NATRON_PLUGIN_PATH=~/natron
-fi
-if [ -f "/Applications/Natron.app/Contents/MacOS/Natron" ]; then
-	alias natron=/Applications/Natron.app/Contents/MacOS/Natron
 fi
 
 # Nuke
@@ -130,12 +105,7 @@ if [ -d "$HOME/OpenColorIO-Configs/aces_1.0.3" ]; then
 	export OCIO=$HOME/OpenColorIO-Configs/aces_1.0.3/config.ocio
 fi
 
-
 # 촬영관련 스크립트
 if [ -f ~/bmpcc/bmpcc.py ]; then
 	alias bmpcc="python ~/bmpcc/bmpcc.py"
 fi
-
-# elo
-export SITE_ROOT=$HOME/.elo
-export SHOW_ROOT=$HOME/project
